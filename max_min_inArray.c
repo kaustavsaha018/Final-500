433295537eb48481a21177248b6324d296c41cc4
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2020-11-02 13:13:59 +0530

    By Kaustav Saha
    Final Commit

---
+++ max_min_inArray.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int find_max(int [],int);
+int find_min(int [],int);
+
+int main(){
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("\nEnter the elements of the array:\n");
+    for(int i=0;i<size;i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("\nMaximum element in the array is: %d", find_max(arr,size));
+    printf("\nMaximum element in the array is: %d", find_min(arr,size));
+    return 0;
+}
+
+int find_max(int arr[],int size){
+    int max=arr[0];
+    for(int i=1;i<size;i++){
+        if(max<arr[i])
+            max=arr[i];
+    }
+    return max;
+}
+int find_min(int arr[],int size){
+    int min=arr[0];
+    for(int i=1;i<size;i++){
+        if(min>arr[i])
+            min=arr[i];
+    }
+    return min;
+}
